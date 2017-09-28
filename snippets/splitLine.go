987f6cbada10f9a0769bdc2f0de8bac217d1a020
yangm11 <mingzhang.yang@nih.gov>
2017-09-28 16:43:45 -0400

    less if else

--- snippets/splitLine.go
+++ snippets/splitLine.go
@@ -2,7 +2,7 @@ package main
 
 import(
 	"fmt"
-	"os"
+	//"os"
 	"strings"
 )
 
@@ -18,31 +18,30 @@ func SplitLine(str, sep string) []string {
 	res := []string{}
 
 	for i := 0; i < len(str); i++ {
-		if state.open {
-			elem += string(str[i])
-			if state.sign == string(str[i]) {
+		c := string(str[i])
+		elem += c
+
+		switch c {
+		case "\"":
+			if state.open {
 				state.open = false
 				state.ready = true
+			} else {
+				state.open = true
+				state.sign = "\""
 			}
-		} else {
-			if string(str[i]) == sep {
-				if state.ready {
-					res = append(res, strings.TrimSpace(elem))
-					state.sign = ""
-					state.ready = false
-					elem = ""
-					continue
-				}
+		case sep:
+			if state.open == false {
 				res = append(res, strings.TrimSpace(elem))
 				elem = ""
 				continue
 			}
-			if string(str[i]) == "\"" {
-				state.open = true
-				state.sign = "\""
-				elem += "\""
+			if state.ready {
+				state.sign = ""
+				state.ready = false
+				res = append(res, strings.TrimSpace(elem))
+				elem = ""
 			}
-			elem += string(str[i])
 		}
 	}
 
@@ -51,8 +50,8 @@ func SplitLine(str, sep string) []string {
 }
 
 func main() {
-	//s := `10,2,tRNA (cytidine(34)-2'-O)-methyltransferase TrmL catalyzes the methyl transfer from S-adenosyl-L-methionine to the ribose at the nucleotide 34 wobble position in the two leucyl isoacceptors tRNA(Leu)(CmAA) and tRNA(Leu)(cmnm5UmAA),tRNA (cytidine(34)-2'-O)-methyltransferase,SpoU_methylase,COG0219,1,1,published,17:04.8,17:04.8,curated`
-	s := os.Args[1]
+	s := `10,2,tRNA (cytidine(34)-2'-O)-methyltransferase TrmL catalyzes the methyl transfer from S-adenosyl-L-methionine to the ribose at the nucleotide 34 wobble position in the two leucyl isoacceptors tRNA(Leu)(CmAA) and tRNA(Leu)(cmnm5UmAA),tRNA (cytidine(34)-2'-O)-methyltransferase,SpoU_methylase,COG0219,1,1,published,17:04.8,17:04.8,curated`
+	//s := os.Args[1]
 	v := SplitLine(s, ",")
 	for i := 0; i < len(v); i++ {
 		fmt.Printf("i = %d, substr: %s\n", i, string(v[i]))
