723aa5bd3e62cb9e7909b123637835ec5fffdb34
yangm11 <mingzhang.yang@nih.gov>
2017-10-16 18:00:22 -0400

    MSI updated

--- algorithm/maximmum_sum_interval.go
+++ algorithm/maximmum_sum_interval.go
@@ -2,24 +2,24 @@ package main
 
 import "fmt"
 
-type Idx struct {
-	start, end int
-}
-
 func MSI(a []int) int {
-	if len(a) == 1 {
-		return a[0]
+	cur := a[0]
+	max := cur
+
+	for i := 1; i < len(a); i++ {
+		if cur < 0 {
+			cur = a[i]
+		} else {
+			cur += a[i]
+		}
+		if max < cur {
+			max = cur
+		}
 	}
-	s := MSI(a[:len(a) - 1])
-	if s < 0 {
-		return a[len(a) - 1]
-	}
-	return  s + a[len(a) - 1]
+	return max
 }
 
 func main() {
 	a := []int{1, 3, 2, -4, 9, -3, 5, 2, 8, -5, 8, 10, -4}
-	for i := len(a); i > 0; i-- {
-		fmt.Println(MSI(a[:i]))
-	}
+	fmt.Println(MSI(a))
 }
