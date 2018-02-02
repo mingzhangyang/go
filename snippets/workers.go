6a1036df66cf6020e74db6648bec9fd5e6cb38c9
Mingzhang Yang <mingzhang.yang@nih.gov>
2018-02-02 18:20:05 -0500

    regular practice

--- snippets/workers.go
+++ snippets/workers.go
@@ -14,35 +14,41 @@ func (j Job) do() {
 	fmt.Println(j.i)
 }
 
-type Worker chan Job
+// type Worker chan Job
 
-func (w Worker) run(q chan bool) {
-	for {
-		select {
-		case j := <-w:
-			j.do()
-		case <-q:
-			return
-		}
-	}
-}
+// func (w Worker) run(q chan bool) {
+// 	for {
+// 		select {
+// 		case j := <-w:
+// 			j.do()
+// 		case <-q:
+// 			return
+// 		}
+// 	}
+// }
 
-func createWorker(n int) Worker {
-	c := make(chan Job, n)
-	return c
-}
+// func createWorker(n int) Worker {
+// 	c := make(chan Job, n)
+// 	return c
+// }
 
-type Pool struct {
-	pool chan Worker
-	n    int
-}
+// type Pool struct {
+// 	pool chan Worker
+// 	n    int
+// }
 
-func (p *Pool) Init(q chan bool) {
-	p.pool = make(chan Worker, p.n)
-	for i := 0; i < p.n; i++ {
-		w := createWorker(10)
-		p.pool <- w
-		go w.run(q)
+// func (p *Pool) Init(q chan bool) {
+// 	p.pool = make(chan Worker, p.n)
+// 	for i := 0; i < p.n; i++ {
+// 		w := createWorker(10)
+// 		p.pool <- w
+// 		go w.run(q)
+// 	}
+// }
+
+func process(c chan Job) {
+	for j := range c {
+		j.do()
 	}
 }
 
@@ -54,19 +60,26 @@ func main() {
 			jobQueue <- Job{i}
 		}
 		close(jobQueue)
-		close(stop) // if do not close stop, all goroutine will keep working for ever. Because closed jobQuequ always emit zero value.
+		// close(stop) // if do not close stop, all goroutine will keep working for ever. Because closed jobQuequ always emit zero value.
 	}()
 
 	// fmt.Println(jobQueue)
-	wp := Pool{n: 10}
-	fmt.Println(wp)
-	wp.Init(stop)
-	fmt.Println(wp)
+	// wp := Pool{n: 10}
+	// fmt.Println(wp)
+	// wp.Init(stop)
+	// fmt.Println(wp)
 
-	for {
-		j := <-jobQueue
-		w := <-wp.pool
-		w <- j
-		wp.pool <- w
+	// for {
+	// 	j := <-jobQueue
+	// 	w := <-wp.pool
+	// 	w <- j
+	// 	wp.pool <- w
+	// }
+
+	for i := 0; i < 10; i++ {
+		go process(jobQueue)
 	}
+
+	<-time.After(time.Second * 15)
+	close(stop)
 }
