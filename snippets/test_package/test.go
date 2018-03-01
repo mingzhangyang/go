a5cf43b13225d3bf331000f098d9ec5ac0ece118
Mingzhang Yang <redondoself@gmail.com>
2018-03-01 10:47:03 -0500

    learning package structure

---
+++ snippets/test_package/test.go
@@ -0,0 +1,20 @@
+package test_package
+
+import "fmt"
+
+type Test struct {
+	x int
+	Y int
+}
+
+func (t Test) GetX() int {
+	return t.x
+}
+
+func (t *Test) SetX(a int) {
+	t.x = a
+}
+
+func (t Test) PrintInfo() {
+	fmt.Printf("x: %d, Y: %d\n", t.x, t.Y)
+}
