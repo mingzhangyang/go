3c5bb74dc3a5a0b6bdc6033931681ef8391325ee
Mingzhang <redondoself@gmail.com>
2018-04-10 21:50:06 -0400

    Create layer.go

---
+++ layer.go
@@ -0,0 +1,44 @@
+package network
+
+// Layer represent a hidden layer of neural network
+type Layer []*Neuron
+
+// NewLayer method return a layer value
+func NewLayer(m, n int) Layer {
+    var l = make([]*Neuron, m)
+    for i := 0; i < m; i++ {
+        l[i] = NewNeuron(n)
+    }
+    return l
+}
+
+// Length method return the length of the layer, which is the number of Neurons in the layer
+func (l Layer) Length() int {
+    return len(l)
+}
+
+// ComputeWithVector method multiply an input vector with the layer
+// this will generate a vector of float64 values with the same length as the layer
+func (l Layer) ComputeWithVector(input []float64) []float64 {
+    var r = make([]float64, len(l))
+    for i := 0; i < len(l); i++ {
+        r[i] = l[i].Compute(input)
+    }
+    return r
+}
+
+// ComputeWithMatrix method multiply a input matrix with the layer
+// this will produce a matrix of float64 values with number of rows equal to the number of records in input
+// and the number of columns equal to the number of Neurons in the layer (the length of the layer)
+func (l Layer) ComputeWithMatrix(input [][]float64) [][]float64 {
+    var r = make([][]float64, len(input))
+    for i := 0; i < len(input); i++ {
+        r[i] = l.ComputeWithVector(input[i])
+    }
+    return r
+}
+
+// Update method update each neuron in the layer to reduce the Loss
+func (l Layer) Update() {
+
+}
