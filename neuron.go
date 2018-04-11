b5016cf339eba599ce1bdcdfbd28c8f23bab47a4
Mingzhang <redondoself@gmail.com>
2018-04-10 21:21:46 -0400

    Create neuron.go
    define the Neuron type, which is the computing unit of neural networks

---
+++ neuron.go
@@ -0,0 +1,43 @@
+package network
+
+import "math/rand"
+
+// Neuron is the computing unit
+type Neuron struct {
+    bias float64
+    weights []float64
+}
+
+// NewNeuron method return the pointer of an initialized neuron ready to use
+func NewNeuron(n int) *Neuron {
+    var neuron Neuron
+    neuron.bias = rand.Float64()
+    s = make([]float64, n)
+    for i := 0; i < n; i++ {
+        s[i] = (rand.Float64() - 0.5) * 2
+    } 
+    neuron.weights = s
+    return &neuron
+}
+
+// NumOfWeights method return the length of the weights slice
+func (n *Neuron) NumOfWeights() int {
+    return len(n.weights)
+}
+
+// Compute method produce the result given an input vector
+func (n *Neuron) Compute(input []float64) float64 {
+    if len(input) != len(n.weights) {
+        panic("The length of input vector doesn't match the weights of the neuron")
+    }
+    var r float64
+    for i := 0; i < len(input); i++ {
+        r += (input[i] * n.weights[i])
+    }
+    return r
+}
+
+// Update method update the weights and bias of the neuron
+func (n *Neuron) Update() {
+
+}
