7361707e2971edc6ca595d750cca6b8acce1025f
Mingzhang <redondoself@gmail.com>
2018-05-07 22:54:02 -0400

    klsjfsd

---
+++ fileprocessing/fmr.go
@@ -0,0 +1,10 @@
+package fileprocessing
+
+import (
+	"regexp"
+)
+
+// DeleteSubString remove all the substring matched to the regexp pattern
+func DeleteSubString(path string, pat *regexp.Regexp, delm byte) {
+
+}
