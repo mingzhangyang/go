9d7035c978e12a44dfeca4f0468ba397c8eebcd5
mingzhang <redondoself@gmail.com>
2018-05-15 13:58:46 -0400

    move hinge loss function to neuralnetwork package

--- models/svm.go
+++ models/svm.go
@@ -1,28 +1,6 @@
 package models
 
 import (
-	"log"
+	// "log"
 )
 
-// The SVM loss function wants the score of the correct class `idx` to
-// be larger than the incorrect class scores by at least by `delta`. If
-// this is not the case, we will accumulate loss.
-func svmLoss(a []float64, idx int, delta float64) float64 {
-	if idx < 0 {
-		idx += len(a)
-	}
-	if idx < 0 || idx >= len(a) {
-		log.Panic("invalid index")
-	}
-	var sum float64
-	var v = a[idx]
-	var d float64
-	for i := 0; i < len(a); i++ {
-		d = a[i] - v + delta
-		if d > 0 {
-			sum += d
-			continue
-		}
-	}
-	return sum - delta
-}
--- neuralnetwork/utilities.go
+++ neuralnetwork/utilities.go
@@ -1,6 +1,9 @@
 package neuralnetwork
 
-import "math"
+import (
+	"log"
+	"math"
+)
 
 // Softmax function for a vector
 func Softmax(v []float64) []float64 {
@@ -121,4 +124,27 @@ func ConvertStringLabelsToFloat64Labels(labels []string) []float64 {
 		count++
 	}
 	return r
+}
+
+// The SVM loss (hinge loss ) function wants the score of the correct class `idx`
+// to be larger than the incorrect class scores by at least by `delta`. If
+// this is not the case, we will accumulate loss.
+func svmLoss(a []float64, idx int, delta float64) float64 {
+	if idx < 0 {
+		idx += len(a)
+	}
+	if idx < 0 || idx >= len(a) {
+		log.Panic("invalid index")
+	}
+	var sum float64
+	var v = a[idx]
+	var d float64
+	for i := 0; i < len(a); i++ {
+		d = a[i] - v + delta
+		if d > 0 {
+			sum += d
+			continue
+		}
+	}
+	return sum - delta
 }
