e9618e0f88e08c710cf423505f3d8038eda722d9
mingzhang <redondoself@gmail.com>
2018-05-15 15:07:46 -0400

    update ANN, add default config

--- neuralnetwork/ann.go
+++ neuralnetwork/ann.go
@@ -1,5 +1,16 @@
 package neuralnetwork
 
+var defultConfig Config
+
+func init() {
+	defultConfig = Config{
+		"activation": "relu",
+	}
+}
+
+// Config is the configuration of one ANN object
+type Config map[string]string
+
 // ANN is the short name of artificial neural network
 type ANN struct {
 	input [][]float64
@@ -14,6 +25,8 @@ type ANN struct {
 // Input set the input field of the ANN struct
 
 // Target set the target field of the ANN struct
+
+// Initialize the hidden layers using the config
 
 // Train optimize the hidden layer (Network) to fit input with output
 
