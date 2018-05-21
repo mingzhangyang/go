6454b064ab7f5f28263fbd9a0a50c4ccde004033
mingzhang <redondoself@gmail.com>
2018-05-21 12:23:20 -0400

    note base created

---
+++ notebase/notebase.go
@@ -0,0 +1,6 @@
+package notebase
+
+/******************************************************************************
+* NoteBase: implementing a toy database for storing notes
+* This is to mimic solr implementation.
+******************************************************************************/
