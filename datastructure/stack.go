9a35c212d982fe681fe330fecf9b3a271afeb86a
Mingzhang <redondoself@gmail.com>
2018-06-07 23:23:20 -0400

    implement stack using slice

--- datastructure/stack.go
+++ datastructure/stack.go
@@ -1,5 +1,9 @@
 package datastructure
 
+import (
+	"fmt"
+)
+
 // Stack type
 type Stack struct {
 	Entry  *Node
@@ -45,4 +49,62 @@ func (s Stack) String() string {
 		str += (" - " + n.Value.(string))
 	}
 	return str
+}
+
+
+// StackA implement stack using slice
+type StackA struct {
+	internal []interface{}
+	head, tail int
+}
+
+// NewStackA create a new StackA and return its pointer
+func NewStackA() *StackA{
+	return &StackA{
+		internal: make([]interface{}, 256),
+		head: 0,
+		tail: 0,
+	}
+}
+
+// IsEmpty tell whether the stack is empty
+func (s *StackA) IsEmpty() bool {
+	return s.head == s.tail
+}
+
+func (s *StackA) extend() {
+	ns := make([]interface{}, 2 * len(s.internal))
+	copy(ns, s.internal)
+	s.internal = ns
+}
+
+// Push an element to the tail of the stack
+func (s *StackA) Push(e interface{}) {
+	if s.tail < len(s.internal) {
+		s.internal[s.tail] = e
+		s.tail++
+		return
+	}
+	s.extend()
+	s.internal[s.tail] = e
+	s.tail++
+}
+
+// Pop remove an element from the tail of the stack
+func (s *StackA) Pop() (interface{}, bool) {
+	if s.IsEmpty() {
+		return nil, false
+	}
+	s.tail--
+	return s.internal[s.tail], true
+}
+
+// Length tell the number of elements in the stack
+func (s *StackA) Length() int {
+	return s.tail - s.head
+}
+
+// String method
+func (s StackA) String() string {
+	return fmt.Sprint(s.internal[s.head:s.tail])
 }
